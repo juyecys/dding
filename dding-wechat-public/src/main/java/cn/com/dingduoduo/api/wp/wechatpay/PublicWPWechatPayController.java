b50bd14c818e58879b9e7d67cf796e210badd89d
cysJeysine <yaojiaxin@chengyisheng.com.cn>
2018-04-22 02:06:38 +0800

    dev course order

--- dding-wechat-public/src/main/java/cn/com/dingduoduo/api/wp/wechatpay/PublicWPWechatPayController.java
+++ dding-wechat-public/src/main/java/cn/com/dingduoduo/api/wp/wechatpay/PublicWPWechatPayController.java
@@ -1,15 +1,11 @@
-package cn.com.dingduoduo.api.wechat.event;
+package cn.com.dingduoduo.api.wp.wechatpay;
 
 import cn.com.dingduoduo.entity.courseorder.CourseOrder;
 import cn.com.dingduoduo.entity.courseorder.CourseOrderDTO;
 import cn.com.dingduoduo.service.courseorder.CourseOrderService;
-import cn.com.dingduoduo.utils.common.Dom4jUtils;
-import cn.com.dingduoduo.service.wechat.event.WechatEventService;
-import cn.com.dingduoduo.untils.wechat.WechatSignUtil;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import com.thoughtworks.xstream.io.xml.XmlFriendlyNameCoder;
-import org.dom4j.DocumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,21 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
 
-
+/**
+ * Created by jeysine on 2018/4/18.
+ */
 @RestController
-@RequestMapping(value = { "/ding/wechat/public/event" }, produces = "application/json")
-public class PublicWechatEventController {
-
-
-    @Autowired
-    private WechatEventService wechatEventService;
+@RequestMapping(value = {"/ding/wp/public/wechat/pay"}, produces = "application/json")
+public class PublicWPWechatPayController {
 
     @Autowired
     private CourseOrderService courseOrderService;
@@ -48,7 +36,7 @@ public class PublicWechatEventController {
 
     private static XStream xStream = new XStream(new DomDriver("UTF-8", new XmlFriendlyNameCoder("-_", "_")));
 
-    private static Logger logger = LoggerFactory.getLogger(PublicWechatEventController.class);
+    private static final Logger logger = LoggerFactory.getLogger(PublicWPWechatPayController.class);
 
     static {
         xStream.ignoreUnknownElements();//忽略多余的xml节点
@@ -71,56 +59,6 @@ public class PublicWechatEventController {
         xStream.aliasField("time_end", WechatPayNotify.class, "timeEnd");
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST,  produces = "application/xml")
-    public void receiveMessage(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // 将请求、响应的编码均设置为UTF-8（防止中文乱码）
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-
-        // 接收参数微信加密签名、 时间戳、随机数
-        String signature = request.getParameter("signature");
-        String timestamp = request.getParameter("timestamp");
-        String nonce = request.getParameter("nonce");
-
-        InputStream inputStream = request.getInputStream();
-
-        PrintWriter out = response.getWriter();
-
-        try {
-            HashMap<String, Object> data = Dom4jUtils.readXml(inputStream);
-            wechatEventService.processEvent(data);
-            logger.debug("微信事件处理结束");
-            out.print("");
-        } catch (DocumentException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            logger.error("read xml error: {}", e);
-        } catch (Exception e) {
-            logger.error("error: {}", e);
-        }
-        out.close();
-        out = null;
-    }
-
-
-    @RequestMapping(value = "/", method = RequestMethod.GET,  produces = "application/json")
-    public void receive(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // 微信加密签名
-        String signature = request.getParameter("signature");
-        // 时间戳
-        String timestamp = request.getParameter("timestamp");
-        // 随机数
-        String nonce = request.getParameter("nonce");
-        // 随机字符串
-        String echostr = request.getParameter("echostr");
-
-        PrintWriter out = response.getWriter();
-        // 请求校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
-        if (WechatSignUtil.checkUserMessageSignature(signature, timestamp, nonce)) {
-            out.print(echostr);
-        }
-        out.close();
-        out = null;
-    }
-
     /**
      * 微信回调支付
      * @param request
@@ -128,7 +66,7 @@ public class PublicWechatEventController {
      * @return
      * @throws Exception
      */
-    @RequestMapping(value = "/pay", method = RequestMethod.POST, produces = "application/xml", consumes = "application/xml")
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = "application/xml", consumes = "application/xml")
     public String wechatPayNotify(@RequestBody String xmlBody, HttpServletRequest request, HttpServletResponse response) throws Exception {
         logger.debug("wechat pay notify: {}", xmlBody);
         String result = "";
@@ -375,5 +313,4 @@ public class PublicWechatEventController {
     public enum ReturnCodeEnum {
         SUCCESS, FAIL;
     }
-
 }
